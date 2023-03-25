4a805cf9beecb6bb74e2b7e6232843dad02b12ed
simsimhan73 <simsimhan73@gmail.com>
2023-03-25 14:28:59 +0900

    settting

---
+++ SHM/Model/Chair.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHM.Model
+{
+    class Chair
+    {
+        private int _row;
+        private int _col;
+        private string _name;
+        private string _note;
+
+        public int Row { get { return _row; } set { _row = value; } }
+        public int Col { get { return _col;} set { _col = value; } }
+        public string Name { get { return _name;} set { _name = value; } }
+        public string Note { get { return _note;} set { _note = value; } }
+    }
+}
